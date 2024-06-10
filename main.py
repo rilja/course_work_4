2fa6afa079f890c20b734a7cc95a183975815223
Artemij Rilev <artik200050@gmail.com>
2024-06-10 15:23:06 +0300

    add user_interaction

--- main.py
+++ main.py
@@ -2,12 +2,34 @@ from src.parser import HH
 from src.saver import JSONSaver
 from src.vacancy import Vacancy
 
-if __name__ == '__main__':
-    hh1 = HH()
-    hh_vacancies = hh1.load_vacancies('python')
+
+# Function for user interaction
+def user_interaction():
+    # asking user for keyword
+    keyword_word = input("Введите ключевое слово для поиска вакансий: ")
+
+    # Creating an instance of a class to work with the API of job sites
+    hh_api = HH()
+
+    # Receiving vacancies from hh.ru in JSON format
+    hh_vacancies = hh_api.load_vacancies(keyword_word)
+
+    # Converting a dataset from JSON to a list of objects
     vacancies_list = Vacancy.cast_to_object_list(hh_vacancies)
+
+    # Saving information about vacancies to a JSON file
     json_saver = JSONSaver()
-    json_saver.add_vacancy(vacancies_list[0])
-    json_saver.add_vacancy(vacancies_list[1])
-    json_saver.add_vacancy(vacancies_list[1])
+    for i in vacancies_list:
+        json_saver.add_vacancy(i)
+
+    top_n = int(input("Введите количество вакансий для вывода в топ N: "))
+
+    salary_range = input("Введите диапазон зарплат: ")  # Пример: 100000 - 150000
+
+    ranged_vacancies = Vacancy.get_vacancies_by_salary(vacancies_list, salary_range)
+    top_vacancies = Vacancy.get_top_vacancies(ranged_vacancies, top_n)
+    print(top_vacancies)
+
+if __name__ == "__main__":
+    user_interaction()
 
